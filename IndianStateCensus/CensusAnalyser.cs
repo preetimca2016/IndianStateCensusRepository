e455246347c7965915173b0d669d1f951ba346af
“preetimca2016” <preetimca2016@gmail.com>
2022-09-22 23:30:51 +0530

    [Preeti] Adding IndianCensusStateInformation files

---
+++ IndianStateCensus/CensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using IndiaStateCensus.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndiaStateCensus
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US
+        }
+        public Dictionary<string, CensusDTO> datamap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            datamap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return datamap;
+        }
+    }
+}
